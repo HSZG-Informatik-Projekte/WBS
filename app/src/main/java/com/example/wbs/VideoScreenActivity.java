eb650969f58bd42fbd4cb482d924a6d5b5962ee1
lenne98 <lenni_fx@freenet.de>
2019-06-02 00:29:44 +0200

    Add VideoScreenActivity and Files

---
+++ app/src/main/java/com/example/wbs/VideoScreenActivity.java
@@ -0,0 +1,56 @@
+package com.example.wbs;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.VideoView;
+
+public class VideoScreenActivity extends AppCompatActivity {
+
+    VideoView videoView;
+    Boolean playing = false;
+    ImageButton playbutton,continuebutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_screen);
+
+        videoView = findViewById(R.id.videoViewID);
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.funny);
+
+        playbutton =findViewById(R.id.playButtonID);
+        continuebutton = findViewById(R.id.continueButtonID);
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                playing=false;
+                playbutton.setVisibility(View.VISIBLE);
+                playbutton.setImageResource(R.drawable.repeat);
+                continuebutton.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    public void play(View view) {
+        if (playing == false) {
+            playbutton.setImageResource(R.drawable.play);
+            videoView.start();
+            playbutton.setVisibility(View.GONE);
+            playing=true;
+        }
+    }
+
+    public void pause(View view){
+        if(playing==true){
+            videoView.pause();
+            playing=false;
+            playbutton.setVisibility(View.VISIBLE);
+        }
+    }
+}
+
