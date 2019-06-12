7e49bd2078f0551227d7af1c9c1d0465774ae874
bytesloth <oneminecraft1@googlemail.com>
2019-06-12 13:03:46 +0200

    q2

--- app/src/main/java/com/example/wbs/EnterNewWorldActivity.java
+++ app/src/main/java/com/example/wbs/EnterNewWorldActivity.java
@@ -24,6 +24,7 @@ public class EnterNewWorldActivity extends AppCompatActivity {
              public void onClick(View v) {
                  Intent mySuperIntent = new Intent(EnterNewWorldActivity.this, VideoScreenActivity.class);
                  mySuperIntent.putExtra("wbsProfile", wbsProfile);
+                 mySuperIntent.putExtra("videoNumber", 0);
                  startActivity(mySuperIntent);
              }
          });
