5e9aaf7f8bd9e36b0e7ea2b9acb4f2c23c3c8023
bytesloth <oneminecraft1@googlemail.com>
2019-06-21 17:35:25 +0200

    Profil ausfüllen check

--- app/src/main/java/com/example/wbs/CreateProfileActivity.java
+++ app/src/main/java/com/example/wbs/CreateProfileActivity.java
@@ -2,11 +2,13 @@ package com.example.wbs;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +16,8 @@ import android.widget.EditText;
 public class CreateProfileActivity extends AppCompatActivity {
 
     private UserProfileClass wbsProfile;
+    String gender = "";
+    String color = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,7 @@ public class CreateProfileActivity extends AppCompatActivity {
         final Button ButtonColor_Green = findViewById(R.id.CPA_button_color_green);
         final Button changeActivityProfile = findViewById(R.id.CPA_button_next);
 
+
         //nextActivity.setEnabled(false);
         TxtName.setText("");
         TxtAge.setText("");
@@ -40,23 +45,37 @@ public class CreateProfileActivity extends AppCompatActivity {
         if (wbsProfile.getisProfile()) {
             TxtName.setText("" + wbsProfile.getName());
             TxtAge.setText("" + wbsProfile.getAge());
-            if (wbsProfile.getGender() == UserProfileClass.Gender.MALE) {
+            if (wbsProfile.getGender().getBezeichner().equals(UserProfileClass.Gender.MALE.getBezeichner())) {
                 ButtonMale.setBackgroundColor(Color.rgb(0x3f, 0x51, 0xb5));
-                ButtonMale.setBackgroundResource(0);
+                ButtonFemale.setBackgroundResource(0);
+                setGenderUsed("FEMALE");
             } else {
                 ButtonFemale.setBackgroundColor(Color.rgb(0xff, 0x69, 0xb4));
                 ButtonMale.setBackgroundResource(0);
+                setGenderUsed("MALE");
+            }
+            switch (wbsProfile.getColor()) {
+                case "#FFFF00": buttonSelected(ButtonColor_Yellow, "#FFFF00");
+                break;
+                case "#FF0000": buttonSelected(ButtonColor_Red, "#FF0000");
+                break;
+                case "#0000FF": buttonSelected(ButtonColor_Blue, "#0000FF");
+                break;
+                case "#00FF00": buttonSelected(ButtonColor_Green, "#00FF00");
+                break;
             }
         }
 
         nextActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                wbsProfile.setisProfile(true);
-                JsonUtil.WBSProfileToJson(CreateProfileActivity.this, wbsProfile);
-                Intent mySuperIntent = new Intent(CreateProfileActivity.this, FollowerChoiceActivity.class);
-                startActivity(mySuperIntent);
-                finish();
+                if (!TxtName.getText().equals("") && !TxtAge.getText().equals("") && !gender.equals("") && !color.equals("")) {
+                    wbsProfile.setisProfile(true);
+                    JsonUtil.WBSProfileToJson(CreateProfileActivity.this, wbsProfile);
+                    Intent mySuperIntent = new Intent(CreateProfileActivity.this, FollowerChoiceActivity.class);
+                    startActivity(mySuperIntent);
+                    finish();
+                }
             }
         });
 
@@ -85,9 +104,11 @@ public class CreateProfileActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {}
         });
 
+
         ButtonFemale.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                setGenderUsed("FEMALE");
                 ButtonFemale.setBackground(Drawable.createFromPath("/drawable/mybuttonborderpink"));
                 ButtonFemale.setBackgroundColor(Color.rgb(0xff, 0x69, 0xb4));//0x3F51B5);
                 ButtonMale.setBackgroundResource(0);
@@ -98,10 +119,10 @@ public class CreateProfileActivity extends AppCompatActivity {
         ButtonMale.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                setGenderUsed("MALE");
                 ButtonMale.setBackgroundColor(Color.rgb(0x3f, 0x51, 0xb5));
                 //ButtonMale.setBackground(Drawable.createFromPath("/drawable/mybuttonborderblue"));
                 //ButtonMale.setBackground(Drawable.createFromPath("android.resource://" + getPackageName() + "/" + getResources().getIdentifier("mybuttonborderblue","drawable",""+getPackageName())));
-
                 ButtonFemale.setBackgroundResource(0);
                 wbsProfile.setGender(UserProfileClass.Gender.MALE);
             }
@@ -109,27 +130,45 @@ public class CreateProfileActivity extends AppCompatActivity {
 
         ButtonColor_Yellow.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                wbsProfile.setColor("#FFFF00");
+            public void onClick(View view) { ;
+                buttonSelected(ButtonColor_Yellow, "#FFFF00");
             }
         });
         ButtonColor_Red.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                wbsProfile.setColor("#FF0000");
+                buttonSelected(ButtonColor_Red, "#FF0000");
             }
         });
         ButtonColor_Blue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                wbsProfile.setColor("#0000FF");
+                buttonSelected(ButtonColor_Blue, "#0000FF");
             }
         });
         ButtonColor_Green.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                wbsProfile.setColor("#00FF00");
+                buttonSelected(ButtonColor_Green, "#00FF00");
             }
         });
+    }
+
+    private void setGenderUsed(String gender) {
+        this.gender = gender;
+    }
+
+    private void buttonSelected(Button b1, String color) {
+        this.color = color;
+        wbsProfile.setColor(color);
+        Button ButtonColor_Yellow = findViewById(R.id.CPA_button_color_yellow);
+        ButtonColor_Yellow.setText("");
+        Button ButtonColor_Red = findViewById(R.id.CPA_button_color_red);
+        ButtonColor_Red.setText("");
+        Button ButtonColor_Blue = findViewById(R.id.CPA_button_color_blue);
+        ButtonColor_Blue.setText("");
+        Button ButtonColor_Green = findViewById(R.id.CPA_button_color_green);
+        ButtonColor_Green.setText("");
+        b1.setText("o");
     }
 }
