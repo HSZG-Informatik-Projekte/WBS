571392c7eb44693fec41d53a711b206587fb2bb5
maxx87 <semmeldreck@gmail.com>
2019-06-22 20:10:49 +0200

    edit NavMenu

--- app/src/main/java/de/hszg/wbs/MainActivity.java
+++ app/src/main/java/de/hszg/wbs/MainActivity.java
@@ -1,11 +1,13 @@
 package de.hszg.wbs;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -24,11 +26,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         wbsProfile = JsonUtil.readProfileFromJson(this);
         videos = JsonUtil.readVideoFromJson(this);
-
-        setContentView(R.layout.activity_main);
 
         mDrawerLayout =(DrawerLayout) findViewById(R.id.menuLayout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
@@ -42,9 +43,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.menu_profile:
+                    case R.id.NAV_Item_profile:
                         Intent mySuperIntent = new Intent(MainActivity.this, ShowProfileActivity.class);
                         startActivity(mySuperIntent);
+                        return true;
+                    case R.id.NAV_Item_follower_shop:
+                    case R.id.NAV_Item_to_do_liste:
+                    case R.id.NAV_Item_settings:
+                        AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                        alert.setTitle("WBS");
+                        alert.setMessage("Dieses Feature ist noch nicht verfügbar!");
+                        alert.setIcon(getResources().getDrawable(R.drawable.ic_menu_info));
+                        alert.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        alert.create();
+                        alert.show();
                         return true;
                     default:
                         return false;
