ab211c89461967732d85fd5986d05937284065af
maxx87 <semmeldreck@gmail.com>
2019-06-22 19:11:00 +0200

    clean - 500ms

--- app/src/main/java/de/hszg/wbs/SplashScreen.java
+++ app/src/main/java/de/hszg/wbs/SplashScreen.java
@@ -4,15 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 public class SplashScreen extends AppCompatActivity {
 
     private UserProfileClass wbsProfile;
 
-    private static int SPLASH_TIME = 1000; //This is 2 seconds
+    private static int SPLASH_TIME = 500; //This is 2 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +29,7 @@ public class SplashScreen extends AppCompatActivity {
                     mySuperIntent = new Intent(SplashScreen.this, CreateProfileActivity.class);
                 }
                 startActivity(mySuperIntent);
-                /* This 'finish()' is for exiting the app when back button pressed
-                 *  from Home page which is ActivityHome
-                 */
+
                 finish();
             }
         }, SPLASH_TIME);
